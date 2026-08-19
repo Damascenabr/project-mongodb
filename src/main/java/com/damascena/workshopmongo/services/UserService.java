85bd1e7b7d665db50cf13e5fa058d5cbf151c81a
Bruno Damascena <damascena.dev@gmail.com>
2026-08-19 12:13:44 -0300

    Contectando ao MongoDB com Repository e service

---
+++ src/main/java/com/damascena/workshopmongo/services/UserService.java
@@ -0,0 +1,18 @@
+package com.damascena.workshopmongo.services;
+
+import com.damascena.workshopmongo.domain.User;
+import com.damascena.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+}
