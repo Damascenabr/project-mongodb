bf38fddac81a99ebe5859fdac47b5d425912d184
Bruno Damascena <damascena.dev@gmail.com>
2026-09-14 19:53:20 -0300

    Consulta com varios criterios

--- src/main/java/com/damascena/workshopmongo/resources/PostResource.java
+++ src/main/java/com/damascena/workshopmongo/resources/PostResource.java
@@ -7,8 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-
-
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -18,19 +18,28 @@ public class PostResource {
     private PostService service;
 
 
-    @RequestMapping(value ="/{id}", method= RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
-    @RequestMapping(value="/titlesearch", method = RequestMethod.GET)
-    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
         text = URL.decodeParam(text);
         List<Post> list = service.findByTitle(text);
         return ResponseEntity.ok().body(list);
     }
 
-
-
+    @RequestMapping(value = "/allsearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> allSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate) throws ParseException {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = service.allSearch(text, min, max);
+        return ResponseEntity.ok().body(list);
+    }
 }
--- src/main/java/com/damascena/workshopmongo/resources/util/URL.java
+++ src/main/java/com/damascena/workshopmongo/resources/util/URL.java
@@ -2,7 +2,10 @@ package com.damascena.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
     public static String decodeParam(String text){
@@ -11,5 +14,17 @@ public class URL {
         }catch (UnsupportedEncodingException e){
             return "";
         }
+    }
+
+    public static Date convertDate(String texDate, Date defaultValue) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        try {
+            return sdf.parse(texDate);
+        } catch (ParseException e) {
+            return defaultValue;
+        }
+
+
     }
 }
